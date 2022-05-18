c4056c4c84a5947a574421c8c711f58f71c727b3
DrNickker <mintagirov2015@yandex.ru>
2022-05-18 11:16:34 +0500

    появление тени в навигации при скроллинге

--- js/main.js
+++ js/main.js
@@ -7,5 +7,12 @@ window.addEventListener('load', () => { /* Страница загружена, 
 })
 
 
+$(window).scroll(function (){
+  var posY=window.scrollY; //переменная сколько уже прокрутили
 
-
+  if (posY > 50){
+        $('.main_nav').addClass('main_nav_one');			
+  } else{
+     $('.main_nav').removeClass('main_nav_one');				
+  }
+});
